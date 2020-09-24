08925130fb028d834cbb9e542fd85d1dd39214da
Senapp Studios <senappstudios@gmail.com>
2020-09-24 18:36:08 +0200

    Improvments

--- scripts/barSpacing.js
+++ scripts/barSpacing.js
@@ -2,16 +2,20 @@ function calculateSpacing() {
   var w = window.innerWidth;
 
   var ulbottombar = document.getElementsByClassName("ulbottombar")[0];
+  var ulbottombarsocials = document.getElementsByClassName(
+    "ulbottombar-socials"
+  )[0];
   var ultopbar = document.getElementsByClassName("ultopbar")[0];
-
   if (w < 525) {
     ultopbar.style.display = "none";
   } else {
     ultopbar.style.display = "unset";
   }
-  if (w < 675) {
+  if (w < 875) {
     ulbottombar.style.display = "none";
+    ulbottombarsocials.style.display = "none";
   } else {
     ulbottombar.style.display = "unset";
+    ulbottombarsocials.style.display = "unset";
   }
 }
