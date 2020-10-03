a7ad6de72bbe2c1b7ab324f703ff935906d63b17
Senapp Studios <senappstudios@gmail.com>
2020-10-04 00:23:26 +0200

    up

--- scripts/barSpacing.js
+++ scripts/barSpacing.js
@@ -21,16 +21,19 @@ function calculateSpacing() {
   if (appcontainer != null) {
     if (w < 550) {
       appcontainer.style.gridTemplateColumns = "auto";
-      bottombarapps.style.bottom = "-700px";
+      bottombarapps.style.bottom = -appcontainer.childElementCount * 300 + "px";
     } else if (w < 825) {
       appcontainer.style.gridTemplateColumns = "auto auto";
-      bottombarapps.style.bottom = "-425px";
+      bottombarapps.style.bottom =
+        -Math.ceil(appcontainer.childElementCount / 2) * 300 + "px";
     } else if (w < 1100) {
       appcontainer.style.gridTemplateColumns = "auto auto auto";
-      bottombarapps.style.bottom = "-150px";
+      bottombarapps.style.bottom =
+        -Math.ceil(appcontainer.childElementCount / 3) * 300 + "px";
     } else {
       appcontainer.style.gridTemplateColumns = "auto auto auto auto";
-      bottombarapps.style.bottom = "-150px";
+      bottombarapps.style.bottom =
+        -Math.ceil(appcontainer.childElementCount / 4) * 300 + "px";
     }
   }
 
