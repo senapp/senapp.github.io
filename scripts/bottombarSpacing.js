ab4584717064e82954103bb9a320f16172034c2d
Senapp Studios <senappstudios@gmail.com>
2020-10-04 21:36:34 +0200

    mobile

--- scripts/bottombarSpacing.js
+++ scripts/bottombarSpacing.js
@@ -1,5 +1,5 @@
 function bottombarSpacing() {
   var bottombar = document.getElementsByClassName("bottombar")[0];
 
-  bottombar.style.bottom = -60 + "px";
+  bottombar.style.bottom = -150 + "px";
 }
